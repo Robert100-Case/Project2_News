a8cde383ed5c0e6d4bd4bde911ac7317c656b88d
Robert Rossman <brossman01@outlook.com>
2021-06-08 16:13:28 -0400

    fix

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-require('dotenv').config();
+//require('dotenv').config();
 
 let sequelize;
 
@@ -7,9 +7,9 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    process.env.DB_NAME='news_search_db',
+    process.env.DB_USER='root',
+    process.env.DB_PASSWORD='99Humble',
     {
       host: 'localhost',
       dialect: 'mysql',
